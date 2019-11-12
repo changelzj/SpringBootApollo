0d6bdfaed13b6dd6a7b77be767e68dcfefc0ab1e
liuzijian <liuzijian@m-tour.com.cn>
2019-11-12 19:56:13 +0800

    add

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,14 +1,12 @@
 package com.example;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableApolloConfig
-public class App {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
 
